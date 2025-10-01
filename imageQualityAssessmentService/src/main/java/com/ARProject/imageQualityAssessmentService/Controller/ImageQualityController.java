7d4ecb39b55d84c3006f4ae0ec731a34dad588aa
AbhiP47 <abhinavjaipanwar@gmail.com>
2025-10-01 11:48:47 +0530

    update config properties

--- imageQualityAssessmentService/src/main/java/com/ARProject/imageQualityAssessmentService/Controller/ImageQualityController.java
+++ imageQualityAssessmentService/src/main/java/com/ARProject/imageQualityAssessmentService/Controller/ImageQualityController.java
@@ -13,7 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 
 @RestController
-@RequestMapping("api/image")
+@RequestMapping("api/imagescore")
 public class ImageQualityController {
 
     private final ImageQualityServiceImpl imageQualityService;
